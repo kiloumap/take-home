baf703115f702f47a11f539d1f0ce2b89d4be3bd
Laurent Martinez <lmartinez@turbulent.ca>
2025-08-22 16:34:40 -0400

    refacto: move migration to shared to avoid duplication and share all migrations

--- backend/src/Shared/Infrastructure/Persistence/Doctrine/Migration/Version20250822203342.php
+++ backend/src/Shared/Infrastructure/Persistence/Doctrine/Migration/Version20250822203342.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace ProductMigrations;
+namespace Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250822172430 extends AbstractMigration
+final class Version20250822203342 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,10 +20,12 @@ final class Version20250822172430 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE pricing_options (name VARCHAR(255) NOT NULL, price NUMERIC(10, 2) NOT NULL, billing_period VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, id UUID NOT NULL, product_id UUID NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE TABLE pricing_options (name VARCHAR(255) NOT NULL, price NUMERIC(10, 2) NOT NULL, billing_period VARCHAR NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, id UUID NOT NULL, product_id UUID NOT NULL, PRIMARY KEY (id))');
         $this->addSql('CREATE INDEX IDX_11CE746D4584665A ON pricing_options (product_id)');
-        $this->addSql("ALTER TABLE pricing_options ADD CONSTRAINT chk_billing_period_valid CHECK (billing_period IN ('monthly', 'yearly', 'weekly', 'lifetime'))");
         $this->addSql('CREATE TABLE products (name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, id UUID NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_B3BA5A5A5E237E06 ON products (name)');
+        $this->addSql('CREATE TABLE users (email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, id UUID NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
         $this->addSql('ALTER TABLE pricing_options ADD CONSTRAINT FK_11CE746D4584665A FOREIGN KEY (product_id) REFERENCES products (id)');
     }
 
@@ -33,5 +35,6 @@ final class Version20250822172430 extends AbstractMigration
         $this->addSql('ALTER TABLE pricing_options DROP CONSTRAINT FK_11CE746D4584665A');
         $this->addSql('DROP TABLE pricing_options');
         $this->addSql('DROP TABLE products');
+        $this->addSql('DROP TABLE users');
     }
 }
